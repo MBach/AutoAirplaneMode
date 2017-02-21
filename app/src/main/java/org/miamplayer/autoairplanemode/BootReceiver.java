dc883c0d938e49ea6e6549b59653879d1b2ea3ad
Matthieu Bachelier <bachelier.matthieu@gmail.com>
2017-02-21 23:26:38 +0100

    Init

---
+++ app/src/main/java/org/miamplayer/autoairplanemode/BootReceiver.java
@@ -0,0 +1,24 @@
+package org.miamplayer.autoairplanemode;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * BootReceiver class reschedule alarms after rebooting.
+ *
+ * @author Matthieu BACHELIER
+ * @since 2017.02
+ * @version 1.0
+ */
+public class BootReceiver extends BroadcastReceiver
+{
+    private final AlarmBroadcastReceiver alarmBroadcastReceiver = new AlarmBroadcastReceiver();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent != null && "android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+            alarmBroadcastReceiver.setAlarms(context);
+        }
+    }
+}
---
+++ app/src/main/java/org/miamplayer/autoairplanemode/Constants.java
@@ -0,0 +1,22 @@
+package org.miamplayer.autoairplanemode;
+
+/**
+ * Constants used in this App.
+ *
+ * @author Matthieu BACHELIER
+ * @since 2017.02
+ * @version 1.0
+ */
+final class Constants
+{
+    static final String APP_IS_ENABLED = "appEnabled";
+    static final String ENABLE_AIRPLANE_TIME = "enableAirplaneTime";
+    static final String DISABLE_AIRPLANE_TIME = "disableAirplaneTime";
+
+    static final int ID_ENABLE = 1;
+    static final int ID_DISABLE = 2;
+
+    static final String ID = "id";
+    static final String END = "end";
+
+}
